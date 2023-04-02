eb7f1b02fdd6b3352f6c0f75994ad203f4300a1c
vladkov15 <vkov2001@gmail.com>
2023-04-03 00:36:03 +0300

    feat: complete form

--- task2-app/src/components/Form/MyForm.tsx
+++ task2-app/src/components/Form/MyForm.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import DatePicker from 'react-datepicker';
+import React, { useEffect, useState } from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
+import { useForm } from 'react-hook-form';
 
 interface FormData {
   firstName: string;
@@ -23,14 +23,30 @@ const MyForm: React.FC<MyFormProps> = ({ onSubmit }) => {
     gender: '',
     image: null,
   });
+  const [refData, refFormData] = useState<FormData>({
+    firstName: '',
+    lastName: '',
+    dateOfBirth: new Date(),
+    country: '',
+    gender: '',
+    image: null,
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    formState,
+  } = useForm<FormData>();
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleDateChange = (date: Date) => {
-    setFormData({ ...formData, dateOfBirth: date });
+  const handleDateChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
   };
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -39,20 +55,7 @@ const MyForm: React.FC<MyFormProps> = ({ onSubmit }) => {
     }
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    if (
-      !formData.firstName 
-      !formData.lastName 
-      !formData.dateOfBirth 
-      !formData.country 
-      !formData.gender ||
-      !formData.image
-    ) {
-      alert('Please fill out required fields.');
-      return;
-    }
+  const handleChange = () => {
     if (!localStorage.forms) {
       localStorage.setItem('forms', JSON.stringify([formData]));
       onSubmit();
@@ -79,47 +82,69 @@ const MyForm: React.FC<MyFormProps> = ({ onSubmit }) => {
     console.log(formData);
   };
 
+  const omSubmit = handleSubmit((data) =>{
+    handleChange()
+  })
+
+  useEffect(() => {
+    if (formState.isSubmitSuccessful) reset();
+  }, [formState, refData, reset]);
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={omSubmit}>
       <label>
         First Name:
         <input
+         {...register('firstName', {
+          required: 'fill in',
+          minLength: { value: 3, message: '>= 3 letters' },
+        })}
           type="text"
           name="firstName"
           value={formData.firstName}
           onChange={handleInputChange}
         />
+        <div className='alert-message'>{errors.firstName?.message}</div>
       </label>
       <br />
       <label>
         Last Name:
-        <input type="text" name="lastName" value={formData.lastName} onChange={handleInputChange} />
+        <input  {...register('lastName', {
+              required: 'fill in',
+              minLength: { value: 3, message: '>= 3 letters' },
+            })} type="text" name="lastName" value={formData.lastName} onChange={handleInputChange} />
+            <div className='alert-message'>{errors.lastName?.message}</div>
       </label>
       <br />
       <label>
         Date of Birth:
-        <DatePicker selected={formData.dateOfBirth} onChange={handleDateChange} />
+        <input {...register('dateOfBirth',{
+              required: 'fill in'})} type='date' name='dateOfBirth' onChange={handleDateChange} />
+             <div className='alert-message'>{errors.dateOfBirth?.message}</div>
       </label>
       <br />
       <label>
         Country:
-        <select name="country" value={formData.country} onChange={handleInputChange}>
+        <select  {...register('country', { required: 'Select at list one option' })} name="country" value={formData.country} onChange={handleInputChange}>
           <option value="">Select a country</option>
           <option value="usa">USA</option>
           <option value="canada">Canada</option>
           <option value="mexico">Mexico</option>
         </select>
+        <div className='alert-message'>{errors.country?.message}</div>
       </label>
       <br />
       <label>
         Gender:
-        <input type="radio" name="gender" value="male" onChange={handleInputChange} /> Male
-        <input type="radio" name="gender" value="female" onChange={handleInputChange} /> Female
+        <input  {...register('gender', { required: 'Select one option' })} type="radio" name="gender" value="male" onChange={handleInputChange} /> Male
+        <input {...register('gender', { required: 'Select one option' })} type="radio" name="gender" value="female" onChange={handleInputChange} /> Female
+        <div className='alert-message'>{errors.gender?.message}</div>
       </label>
       <br />
       <label>
         Image:
-        <input type="file" name="image" onChange={handleImageChange} />
+        <input {...register('image', { required: 'Select file' })} type="file" name="image" onChange={handleImageChange} />
+        <div className='alert-message'>{errors.image?.message}</div>
       </label>
       <br />
       <button type="submit">Submit</button>
